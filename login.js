dbcb76ad84f88cbe8829b3362e9aeb372cc7ce1d
Vignesh Bhandarkar <vigneshbhandarkar2014@gmail.com>
2022-02-03 17:58:49 +0530

    logic change

--- login.js
+++ login.js
@@ -22,7 +22,7 @@ const login = () => {
     }
     
 }
-function commit(){
+function commit(event){
     let form = document.getElementById("form");
     form.addEventListener('submit', (e)=>{
         e.preventDefault()
@@ -47,7 +47,7 @@ let debit1 = Number(debit);
 const debittextnode = document.createTextNode(debit1);
 debitNode.appendChild(debittextnode);
 document.getElementById("mydebit").appendChild(debitNode);
-console.log(typeof(debit1))
+
 
 
 // credit logic
@@ -57,24 +57,58 @@ let credit1 = Number(credit);
 const credittextnode = document.createTextNode(credit1);
 creditNode.appendChild(credittextnode);
 document.getElementById("mycredit").appendChild(creditNode);
-console.log(typeof(credit1))
+
 
 
 // balance logic
 const balanceNode = document.createElement("li");
 
-let balance = document.getElementById("balance").value;
-let balance1 = Number(balance)
+var balance = document.getElementById("bal").innerHTML;
+var balance1 = parseFloat(balance)
 const balancetextnode = document.createTextNode(balance1+credit1-debit1);
 balanceNode.appendChild(balancetextnode);
+
 document.getElementById("mybalance").appendChild(balanceNode);
 
-// document.getElementById("transaction").value = null;
-// document.getElementById("credit").value = null;
-// document.getElementById("debit").value = null;
-// let prevbal = numeric(balanceNode);
-// document.getElementById("balance").value = prevbal;
-// console.log(prevbal)
+console.log(typeof(balance))
+console.log(typeof(balance1))
+console.log(typeof(debit1))
+console.log(typeof(credit1))
+console.log(typeof(balance))
+
+// document.getElementById("transaction").value = "---";
+// document.getElementById("credit").value = b;
+// document.getElementById("debit").value = a;
+// document.getElementById("balance").value = c;
+
+
+//  
 }
+function save(){
+    let name = document.getElementById("transaction").value;
+    let debit = document.getElementById("debit").value;
+let debit1 = Number(debit);
+let credit = document.getElementById("credit").value;
+let credit1 = Number(credit);
+var balance = document.getElementById("bal").innerHTML;
+var balance1 = parseFloat(balance)
+    var data = {
+        transaction: name,
+        dr: debit1,
+        cr: credit1,
+        old: balance1,
+        bal: balance1+credit1-debit1,
+    }
+    var myTimeout = setTimeout(balancevalue, 2000);
+    function balancevalue(){
+        document.getElementById("bal").innerHTML=data.bal 
+    }
+    
+    
+    console.log(data.bal) 
+    console.log(data.dr) 
+    console.log(data.cr) 
+}
+
 
 
